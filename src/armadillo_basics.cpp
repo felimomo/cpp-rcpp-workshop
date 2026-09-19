87bcaaadddb49ce093004b603c9f664965546106
felimomo <felimomouni@gmail.com>
2026-09-19 15:28:17 -0600

    rcpp export

--- src/armadillo_basics.cpp
+++ src/armadillo_basics.cpp
@@ -2,10 +2,13 @@
 // 
 //  -> so that sourceCpp in R knows where to find the 
 //     header 
+// 
+// Also note: [[Rcpp::export]] exports a function
+//            so it's visible from R
 
 #include <RcppArmadillo.h>
 
-
+// [[Rcpp::export]]
 Rcpp::IntegerVector row_col_n(const arma::mat& X) {
   return Rcpp::IntegerVector::create(
     Rcpp::Named("rows") = X.n_rows,
@@ -13,6 +16,7 @@ Rcpp::IntegerVector row_col_n(const arma::mat& X) {
   );
 }
 
+// [[Rcpp::export]]
 Rcpp::BoolVector cumsum_lt_thresh(
 	const arma::vec& v, 
 	const double thresh
