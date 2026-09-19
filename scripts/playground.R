70816a7e59066d2225f28d7a3a0d91ae2a94e8a9
felimomo <felimomouni@gmail.com>
2026-09-19 17:19:52 -0600

    nicer R output

--- scripts/playground.R
+++ scripts/playground.R
@@ -7,9 +7,24 @@
 Rcpp::sourceCpp(here::here("src", "armadillo_basics.cpp"))
 
 # n_row, n_col
+print("Matrix col row number test:")
 Mat <- matrix(
 	rnorm(30, mean=1, sd=0.05),
 	nrow = 6,
 	ncol = 5
 )
 print(row_col_n(Mat))
+
+# cumsum, threshold flags:
+print("Vec cumsum threshold test:")
+v <- runif(10)
+thr <- 1.0
+cat(
+	"vector:", 
+	sprintf("%.2f",v), "\n", sep=', '
+)
+cat(
+	"cumsum:", 
+	sprintf("%.2f",cumsum(v)), "\n", sep=', '
+)
+cat("<=thr.:", cumsum_lt_thresh(v, thr), "\n", sep=', ')
