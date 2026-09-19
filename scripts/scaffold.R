372f28c031fa22a52b31e19f7fe1d5622aec8462
felimomo <felimomouni@gmail.com>
2026-09-19 11:22:20 -0600

    insure [ ;) ] correct R script

--- scripts/scaffold.R
+++ scripts/scaffold.R
@@ -1,5 +1,10 @@
 #!/usr/bin/env Rscript
 
+prefix <- Sys.getenv("CONDA_PREFIX")
+if (!nzchar(prefix) || !startsWith(normalizePath(R.home()), normalizePath(prefix))) {
+  stop("Not running under the insurefit conda env. Run: conda activate insurefit")
+}
+
 path <- path.expand("~/insurefit")
 
 usethis::create_package(path, open = FALSE)
