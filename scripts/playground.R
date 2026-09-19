b77a762e923abeca508a6d1d31414aed54fa6d0a
felimomo <felimomouni@gmail.com>
2026-09-19 15:10:02 -0600

    playground: quick-testing Rcpp armadillo functionalities

---
+++ scripts/playground.R
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+
+# Just a little playground to test out the
+# Armadillo <-> Rcpp <-> R integration
+# Will just pile on quick tests one after the other.
+
+Rcpp::sourceCpp("../src/armadillo_basics.cpp")
+
+# n_row, n_col
+Mat <- matrix(
+	rnorm(30, mean=1, sd=0.05),
+	nrow = 6,
+	ncol = 5
+)
+print(row_col_n(Mat))
