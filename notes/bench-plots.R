e63b1d7627cff5313df9814753f7a8730d259387
felimomo <felimomouni@gmail.com>
2026-09-20 17:02:24 -0600

    added plotting script

---
+++ notes/bench-plots.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+library(dplyr)
+
+df <- read.csv(here::here("notes", "glm-benchmarks.csv"))
+print(head(df))
+
+p <- (
+	ggplot(df, aes(x=n_pred, y=median_s, color=method))
+	+ geom_line()
+	+ facet_wrap(~n_obs, scales="free")
+)
+ggsave(
+	here::here("notes", "runtime.png"), 
+	p, width = 8, height = 5, dpi = 150
+)
+
+p <- (
+	ggplot(df, aes(x=n_pred, y=mem_mb, color=method))
+	+ geom_line()
+	+ facet_wrap(~n_obs, scales="free")
+	+ scale_y_log10()
+)
+ggsave(
+	here::here("notes", "memory.png"), 
+	p, width = 8, height = 5, dpi = 150
+)
